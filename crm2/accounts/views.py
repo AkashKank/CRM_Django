01de0046b1d797faa6b6fc0f36ce12d4ef65380f
Akash Kank <akashkank4@gmail.com>
2024-07-16 18:08:49 +0530

    Add files via upload

--- crm2/accounts/views.py
+++ crm2/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here.
 from .models import *
 from .forms import OrderForm
@@ -34,14 +35,42 @@ def customer(request, pk_test):
     context = {'customer':customer,'orders':orders,'order_count':order_count}
     return render(request,'accounts\customer.html',context)
 
-def createOrder(request):
-
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('Product','status'))
+    customer = Customer.objects.get(id=pk)
+    #form = OrderForm(initial={'customer':customer})
+    formset = OrderFormSet(instance=customer)
     if request.method == 'POST':
         #print('Printing POST:', request.POST)
-        form = OrderForm(request.POST)
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+    
+    context = {'formset':formset}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
+            return redirect('/')
     
+
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
